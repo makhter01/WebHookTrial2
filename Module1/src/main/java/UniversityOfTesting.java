a7da8adea8cdc78f2b64f147cf65e7e9a9ec6d65
makhter01 <34196654+makhter01@users.noreply.github.com>
2018-05-02 12:52:35 -0400

    Update UniversityOfTesting.java

--- Module1/src/main/java/UniversityOfTesting.java
+++ Module1/src/main/java/UniversityOfTesting.java
@@ -7,7 +7,7 @@ public class UniversityOfTesting {
     public static WebDriver driver=null; // webdriver initialization set up
 
     @Test
-    public void setUpMethod(){
+    public void setUpMethod() // this is a setUp method{
         System.getProperty("webdriver.chromedriver","../src/driver/chromedriver");
         driver.get("http://webdriveruniversity.com/");
 
