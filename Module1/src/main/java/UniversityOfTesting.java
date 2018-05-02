da674cc7ed971421c5f244173112d06526f8e777
makhter01 <mehzabinakhter@gmail.com>
2018-05-02 12:31:02 -0400

    1st commit

---
+++ Module1/src/main/java/UniversityOfTesting.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class UniversityOfTesting {
+    public static WebDriver driver=null;
+
+    @Test
+    public void setUp(){
+        System.getProperty("webdriver.chromedriver","../src/driver/chromedriver");
+        driver.get("http://webdriveruniversity.com/");
+
+    }
+}
